259ba4d8e5956a2e638cb7f07038047d263b92a3
Luis Chang <luis.chang@outlook.com>
2021-03-02 00:02:42 -0500

    Init

---
+++ DEVICE.Web/Controllers/SeguridadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEVICE.Web.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string codigo, string contrasena)
+        {
+            return RedirectToAction("Dashboard", "Main", new { area = "Administrativo" });
+        }
+    }
+}
